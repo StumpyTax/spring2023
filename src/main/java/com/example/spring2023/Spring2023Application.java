3878655a3d7a1265f7174ffb945ec8064628ea97
StumpyTax <104196692+StumpyTax@users.noreply.github.com>
2023-09-19 20:26:39 +0500

    :wheelchair:

---
+++ src/main/java/com/example/spring2023/Spring2023Application.java
@@ -0,0 +1,13 @@
+package com.example.spring2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring2023/Spring2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
