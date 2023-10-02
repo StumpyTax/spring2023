2460e5f9890b7669fdec8e4a2632f3393edd32bc
StumpyTax <104196692+StumpyTax@users.noreply.github.com>
2023-10-02 22:21:26 +0500

    :wheelchair:Test

---
+++ src/main/java/com/example/spring2023/ArchUnitTests.java
@@ -0,0 +1,23 @@
+package com.example.spring2023;
+
+
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+
+@AnalyzeClasses(packages = "com.example")
+public class ArchUnitTests {
+    static String PACKAGE_NAME="com.example.spring2023.";
+    @ArchTest
+    static final ArchRule testLayeredArch=layeredArchitecture().consideringAllDependencies()
+                .layer("Domain").definedBy(PACKAGE_NAME+"domain..")
+                .layer("App").definedBy(PACKAGE_NAME+"app..")
+                .layer("Extern").definedBy(PACKAGE_NAME+"extern..")
+
+                .whereLayer("App").mayOnlyBeAccessedByLayers("App","Extern")
+                .whereLayer("Extern").mayOnlyBeAccessedByLayers("Extern");
+    }
+
